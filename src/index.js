79251690016d7b2674a4ef7cc8849b6e3f7e4dd5
thiago orlandini <thiagorlandini@hotmail.com>
2019-12-15 14:31:13 -0300

    tudo ok

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import AutorAdmin from './AutorBox';
+import AutorBox from './AutorBox';
+import LivroBox from './LivroBox';
 import Home from './Home';
 import './index.css';
 import {BrowserRouter as Router, Route,Switch} from 'react-router-dom';
 
 ReactDOM.render((
-        <Router>
+        <Router basename={'/cdc-admin'}>
             <App>
                     <Switch>            
                         <Route exact path="/" component={Home}/>
-                        <Route path="/autor" component={AutorAdmin}/>
-                        <Route path="/livro" />                
+                        <Route path="/autor" component={AutorBox}/>
+                        <Route path="/livro" component={LivroBox}/>                
                     </Switch>            
             </App>
         </Router>
